a702ed85d26c05e789cddf7ab7d3f57238366a3e
Arne (Wahrstorf) <aleitert@kent.edu>
2021-05-01 22:47:26 +0200

    Updated subset graph test to work with exceptions.

--- subsetTest.cpp
+++ subsetTest.cpp
@@ -251,10 +251,27 @@ void testGeneralSSG(ssgFun ssg, int seed, int tests, int maxSize)
         // --- Run test. ---
 
         SubsetTest sst;
+        vector<intPair> answer;
+
         int size = min(i, rand() % maxSize) + 5;
         const Hypergraph& hg = sst.build(size);
-        vector<intPair> answer = ssg(hg);
+
+        try
+        {
+            // Run algorithm.
+            answer = ssg(hg);
+        }
+        catch (const exception& e)
+        {
+            cout << "Test " << i << " failed with exception." << endl;
+            cerr << e.what() << endl;
+
+            allPassed = false;
+            break;
+        }
+
         bool correct = sst.verify(answer);
+
 
         // Cancel if test case fails.
         if (!correct)
