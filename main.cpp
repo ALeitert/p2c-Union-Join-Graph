d5f961c19b50698270081586ba4d1a6356f37103
Arne (T440s) <aleitert@kent.edu>
2021-05-02 19:47:23 +0200

    Added simple test for reduced set and its iterator class.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-#include <stdlib.h>
 #include <unordered_set>
 
 #include "helper.h"
 #include "hypergraph.h"
+#include "reducedSet.h"
 
 using namespace std;
 
@@ -29,8 +29,77 @@ Hypergraph randomHG(int n, int m, int N)
     return Hypergraph(list);
 }
 
-
 int main(int argc, char* argv[])
 {
     cout << "*** Union Join and Subset Graph of Acyclic Hypergraphs ***" << endl;
+
+    srand(123456);
+    int testCases = 10000;
+
+    for (int testNo = 1, perc = -1; testNo < testCases; testNo++)
+    {
+        // --- Create random set. ---
+
+        int maxSize = min(testNo, 1024);
+        int size = rand() % maxSize + 1;
+        vector<int> set;
+
+        // First number.
+        set.push_back(rand() % maxSize);
+
+        for (int i = 0; i < size; i++)
+        {
+            // Next is previous number plus a non-zero amount.
+            // Ensures list is sorted.
+            set.push_back(set.back() + rand() % maxSize + 1);
+        }
+
+
+        // --- Create reduced set. ---
+        ReducedSet rSet(set);
+
+        // Copy stored numbers into a new list.
+        vector<int> itList;
+        for (auto it = rSet.begin(); it != rSet.end(); ++it)
+        {
+            itList.push_back(*it);
+        }
+
+
+        // --- Compare both sets. ---
+
+        for (int i = 0; i < max(set.size(), itList.size()); i++)
+        {
+            if (i >= set.size())
+            {
+                cout << itList[i] << " in list but not original set.";
+                return 1;
+            }
+
+            if (i >= itList.size())
+            {
+                cout << set[i] << " in original set but not in list.";
+                return 1;
+            }
+
+            if (set[i] != itList[i])
+            {
+                cout << "Incorrect number at position " << i << endl;
+                return 1;
+            }
+        }
+
+
+        // --- Print progress. ---
+
+        int progress = (testNo * 100) / testCases;
+
+        if (progress > perc)
+        {
+            perc = progress;
+            cout << perc << " %\r" << flush;
+        }
+
+        if (perc == 100) cout << endl;
+    }
 }
