7ff96f8539935e18b29d7da9d738adc6ffb59728
Arne (Wahrstorf) <aleitert@kent.edu>
2021-05-04 22:21:40 +0200

    Added files for sorting-related functions.

---
+++ algorithms/sorting.h
@@ -0,0 +1,12 @@
+// Contains algorithms and helper functions for sorting (in a broad sense).
+
+#ifndef __Algorithms_Sorting_H__
+#define __Algorithms_Sorting_H__
+
+
+namespace Sorting
+{
+
+}
+
+#endif
