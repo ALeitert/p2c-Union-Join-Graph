75a49ab5176b71dee7eaa92b249db696eec0d503
Arne (Wahrstorf) <aleitert@kent.edu>
2021-05-05 21:43:19 +0200

    Added empty class for heap for Dijkstra-ish algorithm.

---
+++ dataStructures/dijkstraHeap.cpp
@@ -0,0 +1,1 @@
+#include "dijkstraHeap.h"
---
+++ dataStructures/dijkstraHeap.h
@@ -0,0 +1,17 @@
+// Implements a heap for Dijkstra's algorithm.
+// Allows to quickly find vertices and update their weight.
+
+#ifndef __DijkstraHeap_H__
+#define __DijkstraHeap_H__
+
+
+class DijkstraHeap
+{
+public:
+
+
+private:
+
+};
+
+#endif
