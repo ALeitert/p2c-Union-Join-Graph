9fda3339d8b9902a20c043e0a16b4942cfdc7f8c
Arne (Wahrstorf) <aleitert@kent.edu>
2021-05-05 17:28:38 +0200

    Added file and namespace for MST-Edge algorithms.

---
+++ algorithms/mstEdges.h
@@ -0,0 +1,12 @@
+// Provides algorithms to compute all edges of a graph which are part of a maximum spanning tree.
+
+#ifndef __Algorithms_MstEdges_H__
+#define __Algorithms_MstEdges_H__
+
+
+namespace MstEdges
+{
+
+}
+
+#endif
