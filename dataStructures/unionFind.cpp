c47759783548146cc43fa6f9151d0d7dd2a4b8cc
Arne (Wahrstorf) <aleitert@kent.edu>
2021-05-06 00:49:18 +0200

    Added empty class for Union-Find data structure.

---
+++ dataStructures/unionFind.cpp
@@ -0,0 +1,1 @@
+#include "unionFind.h"
---
+++ dataStructures/unionFind.h
@@ -0,0 +1,15 @@
+// Implements a Union-Find data structure.
+
+#ifndef __UnionFind_H__
+#define __UnionFind_H__
+
+class UnionFind
+{
+public:
+
+
+private:
+
+};
+
+#endif
