c35d100d900e59c95afc4c41d6c021fba5c28419
Arne (Wahrstorf) <aleitert@kent.edu>
2021-05-05 14:21:20 +0200

    Added empty Graph class.

---
+++ dataStructures/graph.h
@@ -0,0 +1,18 @@
+// Represents a weighted undirected graph.
+
+#ifndef __Graph_H__
+#define __Graph_H__
+
+
+// Represents a weighted undirected graph.
+class Graph
+{
+
+public:
+
+
+private:
+
+};
+
+#endif
