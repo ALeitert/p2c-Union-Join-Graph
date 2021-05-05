38080d7751db8ec8388d170020ba7c964c8af596
Arne (Wahrstorf) <aleitert@kent.edu>
2021-05-06 01:00:41 +0200

    Added fields, constructors, and destructor to Union-Find data structure.

--- dataStructures/unionFind.cpp
+++ dataStructures/unionFind.cpp
@@ -1,1 +1,27 @@
 #include "unionFind.h"
+
+
+// Default constructor.
+// Creates an empty data structure.
+UnionFind::UnionFind() { /* Does nothing. */ }
+
+// Default constructor.
+// Creates an empty data structure.
+UnionFind::UnionFind(size_t size) : size(size)
+{
+    parent = new size_t[size];
+    rank = new size_t[size];
+
+    for (size_t i = 0; i < size; i++)
+    {
+        parent[i] = i;
+        rank[i] = 0;
+    }
+}
+
+// Destructor.
+UnionFind::~UnionFind()
+{
+    if (rank != nullptr) delete[] rank;
+    if (parent != nullptr) delete[] parent;
+}
--- dataStructures/unionFind.h
+++ dataStructures/unionFind.h
@@ -3,13 +3,36 @@
 #ifndef __UnionFind_H__
 #define __UnionFind_H__
 
+
+#include <cstddef>
+
+
 class UnionFind
 {
 public:
 
+    // Default constructor.
+    // Creates an empty data structure.
+    UnionFind();
+
+    // Default constructor.
+    // Creates an empty data structure.
+    UnionFind(size_t size);
+
+    // Destructor.
+    ~UnionFind();
+
 
 private:
 
+    // Number of elements in data structure.
+    size_t size = 0;
+
+    // Stores the rank of each element.
+    size_t* rank = nullptr;
+
+    // Stores the parent of each element.
+    size_t* parent = nullptr;
 };
 
 #endif
