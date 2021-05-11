1fbabd9d47189ade0beda6b0eb82c29df65a9fb2
Arne (Wahrstorf) <aleitert@kent.edu>
2021-05-11 19:36:55 +0200

    Added constructor to MaxCardinalitySet that is based on total number of sets.

--- dataStructures/maxCardinalitySet.cpp
+++ dataStructures/maxCardinalitySet.cpp
@@ -4,3 +4,21 @@
 // Default constructor.
 // Creates empty set.
 MaxCardinalitySet::MaxCardinalitySet() { /* Do nothing. */ }
+
+// Contructor.
+// Creates the data structure with the given number of sets.
+MaxCardinalitySet::MaxCardinalitySet(size_t k)
+{
+    // --- Create k empty sets. ---
+
+    setList.resize(k);
+    setInfo.resize(k);
+
+    for (int i = 0; i < k; i++)
+    {
+        setList[i] = i;
+        setInfo[i] = sizePair(i, 0);
+    }
+
+    endIndices.push_back(k);
+}
--- dataStructures/maxCardinalitySet.h
+++ dataStructures/maxCardinalitySet.h
@@ -16,6 +16,14 @@ class MaxCardinalitySet
 {
 public:
 
+    // Default constructor.
+    // Creates empty set.
+    MaxCardinalitySet();
+
+    // Contructor.
+    // Creates the data structure with the given number of sets.
+    // Set IDs are in [0, k). Each set has size 0.
+    MaxCardinalitySet(size_t k);
 
 private:
 
@@ -29,6 +37,5 @@ private:
     vector<size_t> startIndices;
 
 };
-
 
 #endif
