f3c752e87fcc0b3884ec4c34f09b081a81d83f5d
Arne (Wahrstorf) <aleitert@kent.edu>
2021-05-11 19:19:26 +0200

    Added (empty) class to manage sets for max cardinality search.

---
+++ dataStructures/maxCardinalitySet.h
@@ -0,0 +1,17 @@
+// Implements a data structure to mange sets for maximum cardinality search.
+
+#ifndef __MaxCardinalitySet_H__
+#define __MaxCardinalitySet_H__
+
+
+class MaxCardinalitySet
+{
+public:
+
+
+private:
+
+};
+
+
+#endif
