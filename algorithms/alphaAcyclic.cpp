9c085ce18802d06e2cc52f4852a85caf8b37004f
Arne (T440s) <aleitert@kent.edu>
2021-05-11 04:49:48 +0200

    Added generation of random tree.

--- algorithms/alphaAcyclic.cpp
+++ algorithms/alphaAcyclic.cpp
@@ -1,4 +1,5 @@
 #include "alphaAcyclic.h"
+#include "sorting.h"
 
 
 // Genrates an alpha-acyclic hypergraph with m edges and total size N.
@@ -18,4 +19,26 @@ Hypergraph AlphaAcyclic::genrate(size_t m, size_t N)
     //   * Pick s random vertices from parent.
     //   * Add these to child and then fill it with new vertices.
     // - Shuffle IDs and create hypergraph.
+
+
+    // --- Generate a random tree. ---
+
+    // Random permutation.
+    int edgeIds[m];
+    Sorting::makePermutation(edgeIds, m);
+
+    // Set parents.
+    int rootId = edgeIds[0];
+    int parIds[m];
+    parIds[rootId] = -1;
+
+    for (size_t i = 1; i < m; i++)
+    {
+        int eId = edgeIds[i];
+
+        int pIdx = rand() % i;
+        int pId = edgeIds[pIdx];
+
+        parIds[eId] = pId;
+    }
 }
