7a175dc03bd4cfdd8e7a61c025c749e84a9b2cca
Arne (T440s) <aleitert@kent.edu>
2021-05-11 04:59:54 +0200

    Added distribution of vertices.

--- algorithms/alphaAcyclic.cpp
+++ algorithms/alphaAcyclic.cpp
@@ -59,4 +59,48 @@ Hypergraph AlphaAcyclic::genrate(size_t m, size_t N)
         size_t eId = rand() % m;
         eSize[eId]++;
     }
+
+
+    // --- Determine shared vertices between hyperedges and their parenst. ---
+
+    // List of vertices in each hyperedge.
+    vector<int> vLists[m];
+
+    // Total number of vertices.
+    size_t n = 0;
+
+    // Vertices in the root.
+    for (; n < eSize[rootId]; n++)
+    {
+        vLists[rootId].push_back(n);
+    }
+
+    // Remaining hyperedges.
+    for (size_t i = 1; i < m; i++)
+    {
+        int eId = edgeIds[i]; // current hyperedge
+        int pId = parIds[eId]; // parent
+
+        size_t eS = eSize[eId];
+        size_t pS = eSize[pId];
+
+        vector<int>& pList = vLists[pId];
+        vector<int>& eList = vLists[eId];
+
+        // Number of shared vertices.
+        size_t shared = rand() % min(eS, pS) + 1;
+        Sorting::kShuffle(pList, shared);
+
+        // Add shared vertices.
+        for (size_t j = 0; j < shared; j++)
+        {
+            eList.push_back(pList[j]);
+        }
+
+        // Add remaining new vertices.
+        for (size_t j = shared; j < eS; j++, n++)
+        {
+            eList.push_back(n);
+        }
+    }
 }
