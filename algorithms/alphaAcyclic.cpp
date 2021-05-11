9dbf0ba9dc72090a38ac239d2239654d829bfce1
Arne (T440s) <aleitert@kent.edu>
2021-05-11 04:34:10 +0200

    Added files for AlphaAcyclic namespace.

---
+++ algorithms/alphaAcyclic.cpp
@@ -0,0 +1,1 @@
+#include "alphaAcyclic.h"
---
+++ algorithms/alphaAcyclic.h
@@ -0,0 +1,12 @@
+// Contains algorithms for alpha-acyclic hypergraphs.
+
+#ifndef __Algorithms_AlphaAcyclic_H__
+#define __Algorithms_AlphaAcyclic_H__
+
+
+namespace AlphaAcyclic
+{
+
+}
+
+#endif
