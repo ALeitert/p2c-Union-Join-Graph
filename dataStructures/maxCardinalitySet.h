25887f1c8b57620b38e815960cc83a4d562b0df0
Arne (Wahrstorf) <aleitert@kent.edu>
2021-05-11 19:28:02 +0200

    Added private fields to MaxCardinalitySet class.

--- dataStructures/maxCardinalitySet.h
+++ dataStructures/maxCardinalitySet.h
@@ -4,12 +4,29 @@
 #define __MaxCardinalitySet_H__
 
 
+#include <vector>
+
+#include "../helper.h"
+
+
+using namespace std;
+
+
 class MaxCardinalitySet
 {
 public:
 
 
 private:
+
+    // Contains the stored sets in order of their size.
+    vector<int> setList;
+
+    // States the index and size of each set.
+    vector<sizePair> setInfo;
+
+    // The start indices of each group of equal size.
+    vector<size_t> startIndices;
 
 };
 
