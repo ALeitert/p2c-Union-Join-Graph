ebaed2e501eb07743707f1fa98f0930c5e61d97c
Arne (Wahrstorf) <aleitert@kent.edu>
2021-05-13 23:10:03 +0200

    Added empty class to represent tests.

---
+++ testing/unionJoinTest.h
@@ -0,0 +1,22 @@
+// Provides a framework to test algorithms for Union Join Graphs.
+
+#ifndef __Testing_UnionJoinTest_H__
+#define __Testing_UnionJoinTest_H__
+
+
+namespace Testing
+{
+    class UnionJoinTest
+    {
+    public:
+
+        // Default constructor.
+        UnionJoinTest() { /* Does nothing */ }
+
+
+    private:
+
+    };
+}
+
+#endif
