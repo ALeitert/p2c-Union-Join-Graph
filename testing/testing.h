1c2d28eea6c8b7c32934d5b3e461124d053389e7
Arne (Wahrstorf) <aleitert@kent.edu>
2021-05-15 19:08:58 +0200

    Added function to measure runtime of algorithm.

--- testing/testing.h
+++ testing/testing.h
@@ -5,12 +5,22 @@
 
 
 #include "../algorithms/subsetGraph.h"
+#include "unionJoinTest.h"
 
 
 namespace Testing
 {
+    // --- Subset Graph ---
+
     // Tests a given function that computes the subset graph of a given hypergraph.
     void testGeneralSSG(SubsetGraph::ssgAlgo ssg, int seed, int tests, int maxSize);
+
+
+    // --- Union Join Graph ---
+
+    // Tests a given function that computes the subset graph of a given hypergraph.
+    // Returns the time needed in milliseconds.
+    uint64_t unionJoinGraph(ujgAlgo algo, string name, unsigned int seed, size_t tests, size_t maxSize);
 }
 
 #endif
