a9f483c2f10caf6c62dd620bc8a478ddb4ff02de
Arne (Wahrstorf) <aleitert@kent.edu>
2021-05-16 21:27:37 +0200

    Added files for Partition-Refinement data structure.

---
+++ dataStructures/partRefine.cpp
@@ -0,0 +1,1 @@
+#include "partRefine.h"
---
+++ dataStructures/partRefine.h
@@ -0,0 +1,17 @@
+// Implements a Partition-Refinement data structure.
+
+#ifndef __PartRefine_H__
+#define __PartRefine_H__
+
+
+class PartRefinement
+{
+public:
+
+
+private:
+
+
+};
+
+#endif
