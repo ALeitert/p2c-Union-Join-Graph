bbbb7279ad2ba83649349bd20d06551532b5a43d
Arne (Wahrstorf) <aleitert@kent.edu>
2021-05-16 21:38:24 +0200

    Added helper struct to represent groups within a partition refinement.

--- dataStructures/partRefine.h
+++ dataStructures/partRefine.h
@@ -4,8 +4,28 @@
 #define __PartRefine_H__
 
 
+#include <cstdlib>
+
+
 class PartRefinement
 {
+    // Represents a group within the partition refinement.
+    struct Group
+    {
+        // The index of the first element in the group (inclusive).
+        size_t start = 0;
+
+        // The index of the last element in the group (inclusive).
+        size_t end = 0;
+
+        // The index of the next group.
+        size_t nextId = 0;
+
+        // A temporary counter how many elements will be removed from the group.
+        size_t count = 0;
+    };
+
+
 public:
 
 
