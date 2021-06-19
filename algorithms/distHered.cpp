0a17a402c00f85a130c743bffd49f65e3f791340
Arne (Wahrstorf) <aleitert@kent.edu>
2021-06-20 01:43:38 +0200

    Added outline of algorithm that checks if given graph is cograph.

--- algorithms/distHered.cpp
+++ algorithms/distHered.cpp
@@ -30,6 +30,29 @@ namespace
         // ToDo: return type.
         throw runtime_error("Not implemented.");
 
-        // Algorithm 5 from [2].
+
+        // --- Algorithm 5 from [2] ---
+
+        //  0  Compute a factorising permutation sigma = < v_1, v_2, ..., v_n >.
+
+        //  1  Add v_0 and v_{n + 1} to sigma (these are dummy vertices which
+        //     are never twins with any other vertex).
+        //  2  Set z := v_1.
+        //  3  Let pre(z) and suc(z) denote the preceeding and succeding
+        //     vertices of z in sigma, respectively.
+
+        //  4  While z != v_{n + 1}
+        //  5      If z and pre(z) are twins (true or false) in G(sigma) Then
+        //  6          Remove pre(z) from sigma.
+        //  7      Else If z and suc(z) are twins in G(sigma) Then
+        //  8          Set z := suc(z).
+        //  9          Remove pre(z) from sigma.
+        // 10      Else
+        // 11          Set z := suc(z).
+
+        // 12  If |sigma - { v_0, v_{n + 1} }| = 1 Then
+        // 13      G is a cograph.
+        // 14  Else
+        // 15      G contains a P_4.
     }
 }
